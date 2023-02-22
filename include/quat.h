0ec8c633bc876c01913c100daed340e99f251b3a
Rasmus Hallbäck <rasmushallback01@gmail.com>
2023-02-22 22:46:22 +0100

    Quaternion normalization and slerp

--- include/quat.h
+++ include/quat.h
@@ -14,9 +14,12 @@ namespace gesu {
 
             //static construction functions
             static Quat from_euler(Vec3 euler_values);
+            static Quat slerp(Quat qa, Quat qb, float t);
 
             //instance functions
             Vec3 to_euler();
+
+            void normalize();
 
             //variables
             float w;
--- src/quat.cpp
+++ src/quat.cpp
@@ -39,4 +39,29 @@ namespace gesu {
             return {h, p, b};
         }
     }
+
+    Quat Quat::slerp(Quat q1, Quat q2, float t) {
+        double cos_half_theta = q1.w * q2.w + q1.x * q2.x + q1.y * q2.y + q1.z * q2.z;
+        if (abs(cos_half_theta) >= 1.0){
+            return Quat(q1.w, q1.x, q1.y, q1.z);
+        }
+
+        double half_theta = acos(cos_half_theta);
+        double sinhalf_theta = sqrt(1.0 - cos_half_theta*cos_half_theta);
+        if (fabs(sinhalf_theta) < 0.001){
+            return Quat(q1.w * 0.5 + q2.w * 0.5, q1.x * 0.5 + q2.x * 0.5, q1.y * 0.5 + q2.y * 0.5, q1.z * 0.5 + q2.z * 0.5);
+        }
+
+        double ratio_1 = sin((1 - t) * half_theta) / sinhalf_theta;
+        double ratio_2 = sin(t * half_theta) / sinhalf_theta; 
+        return Quat(q1.w * ratio_1 + q2.w * ratio_2, q1.x * ratio_1 + q2.x * ratio_2, q1.y * ratio_1 + q2.y * ratio_2, q1.z * ratio_1 + q2.z * ratio_2);
+    }
+
+    void Quat::normalize() {
+        float norm = sqrt(x*x + y*y + z*z + w*w);
+        x /= norm;
+        y /= norm;
+        z /= norm;
+        w /= norm;
+    }
 }
