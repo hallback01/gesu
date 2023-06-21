ce7f4cc42af3a3e9183b9b5a1655eb0561025172
Rasmus Hallbäck <68335712+hallback01@users.noreply.github.com>
2023-06-21 21:05:02 +0200

    Update vec2.cpp

--- src/vec2.cpp
+++ src/vec2.cpp
@@ -17,15 +17,15 @@ namespace gesu {
         y = _y;
     }
 
-    Vec2 Vec2::operator-(const Vec2& other) {
+    Vec2 Vec2::operator-(Vec2 other) {
         return {x - other.x, y - other.y};
     }
 
-    Vec2 Vec2::operator+(const Vec2& other) {
+    Vec2 Vec2::operator+(Vec2 other) {
         return {x + other.x, y + other.y};
     }
 
-    float Vec2::operator*(const Vec2& other) {
+    float Vec2::operator*(Vec2 other) {
         return x*other.x + y*other.y;
     }
 
